6b87ed8a4930240f74dddadc3dc780a45b583ec5
meltonlogan615 <meltonlogan@icloud.com>
2020-04-30 15:36:51 -0500

    Re-Reinstalling Tailwind

--- src/components/Heading.js
+++ src/components/Heading.js
@@ -9,15 +9,15 @@ const Heading = () => {
     }, 800)
 
     return (
-        <div className="main-header">
-            <h1>welcome to
+        <div className="main-header justify-center align-center">
+            <h1 className="text-4xl font-normal">Welcome to
                 <br></br>
                 FreeCodeCamp
                 <br></br>
                 Nashville
             </h1>
         </div>
-        )
+    )
 }
 
 export default Heading
