f7e6c6cca1ecc58c675db4bc703f9f85838e4ecb
James McC <mariobud@gmail.com>
2020-04-29 21:19:15 -0500

    Reverted back to HTTP

--- src/Helpers/EventManager.js
+++ src/Helpers/EventManager.js
@@ -3,7 +3,10 @@
 //In order to access the mockup data, type 
 //"json-server -p 5002 -w mockAPI.json"
 //in a seperate terminal of your choice.
-const remoteURL = "http://localhost:5002"
+
+
+//TODO: Add a try/catch where HTTPS fails to use HTTP
+const remoteURL = "http://localhost:3001"
 
 export default {
   //Gets one event info based on ID.
