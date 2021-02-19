ad2db17e1fded13eb07fe86e00294ca41a385e17
hellrungj <johnjameshellrung@gmail.com>
2021-02-18 23:38:30 -0600

    Mapped the tailwind styles in the css and added a few tailwind styles for the header

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,25 @@ module.exports = {
   purge: [
     './src/**/*.js',
   ],
-  theme: {},
+  theme: {
+    extend: {
+      fontFamily: {
+        'nunito': ['Pacifico', 'cursive'],
+        'MyFont': ['Roboto', 'sans-serif'] // Ensure fonts with spaces have " " surrounding it.
+      },
+      colors: {
+        FCCorange: '#F68B1F',
+        FCCblue: {
+          100: '#E3F6F8',
+          200: '#5AC2CD',
+        },
+        FCCgray: {
+          100: '#999999',
+          200: '#444444',
+        },
+      },
+    },
+  },
   variants: {},
   // https://github.com/tailwindcss/custom-forms
   plugins: [require("@tailwindcss/custom-forms")],
