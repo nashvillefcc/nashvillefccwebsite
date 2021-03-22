e7f9ca5e84d7a04dd46c47f605fb30df61a60e2d
Peter Hurlburt <moribundmedium@gmail.com>
2021-03-22 15:30:57 -0500

    Refactored contents of main page to resemble mock-up

--- src/components/layout.js
+++ src/components/layout.js
@@ -8,12 +8,10 @@ function Layout({ children }) {
     <div className="flex flex-col min-h-screen font-sans text-gray-900">
       <Header />
 
-      <main className="flex-1 w-full max-w-4xl px-4 py-8 mx-auto md:px-8 md:py-16">
-        {children}
-      </main>
+      <main className="flex-1">{children}</main>
 
       <footer className="bg-blue-700">
-        <nav className="flex justify-between max-w-4xl p-4 mx-auto text-sm md:p-8">
+        <nav className="flex justify-between max-w-5xl p-4 mx-auto text-sm md:p-8">
           <p className="text-white">This is a footer.</p>
           <p className="text-white">We have no use for it yet!</p>
         </nav>
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,118 +1,70 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import Img from 'gatsby-image';
+import CommunityLogo from '../assets/Community-Learning.svg';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
-import styles from './index.module.css';
 
 function IndexPage() {
-  const data = useStaticQuery(graphql`
-    query {
-      placeholderImageOne: file(
-        relativePath: { eq: "30-photos-absurdes-banques-dimages-23.jpg" }
-      ) {
-        childImageSharp {
-          fixed(width: 200, height: 200) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-      placeholderImageTwo: file(
-        relativePath: { eq: "feet-face-599c412c22fa3a0011d92add.jpg" }
-      ) {
-        childImageSharp {
-          fixed(width: 200, height: 200) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-      placeholderImageThree: file(relativePath: { eq: "332260066.jpg" }) {
-        childImageSharp {
-          fixed(width: 200, height: 200) {
-            ...GatsbyImageSharpFixed
-          }
-        }
-      }
-    }
-  `);
-
   return (
     <Layout>
       <SEO keywords={[`nashville`, `freecodecamp`]} title="Home" />
 
-      <section className="text-center mb-16">
-        <h2 className="inline-block p-3 mb-4 text-4xl font-bold bg-yellow-400">
-          Welcome to Nashville! We code here.
-        </h2>
+      <section className="main-section md:bg-FCCblue-100">
+        <div className="main-section-content main-section-index-1">
+          <div className="pt-8 px-2 md:px-0 md:mr-8 md:w-5/12">
+            <h2>
+              <span>freeCodeCamp Nashville</span>
+              <span className="inline md:hidden">:</span>
+            </h2>
 
-        <p className="text-left leading-loose">
-          Nashville freeCodeCamp is an affiliation of coders, programmers,
-          developers and designers with an eye toward helping others break into
-          the tech industry. Our goal is to foster a welcoming community of
-          learners, by grouping up more experienced mentors with students.
-        </p>
-      </section>
-
-      <section className="text-center mb-16">
-        <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
-          Links
-        </h2>
-
-        <p className="text-left leading-loose">
-          We organize weekly meetings under <a href="#">this Meetup account</a>.
-        </p>
-
-        <p className="text-left leading-loose">
-          We communicate regularly through <a href="#">our Discord channel</a>.
-        </p>
-
-        <p className="text-left leading-loose">
-          We have a number of project repositories under{' '}
-          <a href="#">our GitHub account</a>.
-        </p>
-      </section>
-
-      <section className="text-center mb-16">
-        <h2 className="inline-block p-3 mb-4 text-2xl font-bold bg-yellow-400">
-          Leadership
-        </h2>
-
-        <div className="grid grid-cols-3 gap-2">
-          <div className="py-4 bg-gray-200 border-2 border-gray-400 rounded">
-            <Img
-              className={styles.profilePhoto + ` mx-8 my-4 w-1/4`}
-              fixed={data.placeholderImageOne.childImageSharp.fixed}
-              alt="Professor Xavier"
-            />
-            <p className="text-xl font-bold">Professor Xavier</p>
-            <p className="italic">Organization founder</p>
+            <p className="mb-5">
+              The Nashville chapter of FreeCodeCamp, a non-profit organization
+              offering free online coding curriculum, is a thriving community of
+              developers, designers and students in Middle Tennessee.
+            </p>
             <p>
-              <a href="#">GitHub profile</a>
+              We welcome coders of all varieties: never wrote a line of code to
+              senior-level looking to give back. Follow us on our social media
+              platforms or join us at one of our meetings.
             </p>
           </div>
-          <div className="py-4 bg-gray-200 border-2 border-gray-400 rounded">
-            <Img
-              className={styles.profilePhoto + ` mx-8 my-4 w-1/4`}
-              fixed={data.placeholderImageTwo.childImageSharp.fixed}
-              alt="Wolverine"
+
+          <div className="flex-auto">
+            <CommunityLogo
+              className="inline-block"
+              alt="Community: learning together"
             />
-            <p className="text-xl font-bold">Wolverine</p>
-            <p className="italic">Canadian</p>
+          </div>
+        </div>
+      </section>
+
+      <section className="FCC-divider">
+        <h2>
+          <span className="hidden md:inline">UPCOMING</span> <span>EVENTS</span>
+        </h2>
+      </section>
+
+      <section className="main-section">
+        <div className="main-section-content grid grid-cols-3 gap-2">
+          <div className="p-4 border-2 border-gray-400">
+            <p className="text-xl font-bold">(This section is a WIP)</p>
+            <p className="italic">Meetup Title</p>
             <p>
-              <a href="#">GitHub profile</a>
+              <a href="#">Meetup Location</a>
             </p>
           </div>
-          <div className="py-4 bg-gray-200 border-2 border-gray-400 rounded">
-            <Img
-              className={styles.profilePhoto + ` mx-8 my-4 w-1/4`}
-              fixed={data.placeholderImageThree.childImageSharp.fixed}
-              alt="Cyclops"
-            />
-            <p className="text-xl font-bold">Cyclops</p>
-            <p className="italic">Wise guy</p>
+          <div className="p-4 border-2 border-gray-400">
+            <p className="text-xl font-bold">Meetup Date/Time</p>
+            <p className="italic">Meetup Title</p>
             <p>
-              <a href="#">GitHub profile</a>
+              <a href="#">Meetup Location</a>
+            </p>
+          </div>
+          <div className="p-4 border-2 border-gray-400">
+            <p className="text-xl font-bold">Meetup Date/Time</p>
+            <p className="italic">Meetup Title</p>
+            <p>
+              <a href="#">Meetup Location</a>
             </p>
           </div>
         </div>
