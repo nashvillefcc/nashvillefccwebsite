183c27368c6b015a8dbd59b64959819a0e406b49
Reid Fuhrman <reidjf77@gmail.com>
2021-04-18 22:58:59 -0400

    Implemented footer design.
    - Added fontawesome icons
    - Added the Connect logo and solved and issue where some letters were not showing up.
    - Added all relevant social media links
    - Added Seth's email as the temporary contact email until the domain is setup.
    - Deploys by netlify graphic added into the footer.

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,48 +1,48 @@
 import React from 'react'
 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope } from '@fortawesome/free-solid-svg-icons'
+import { faMeetup, faDiscord, faSlack, faGithub, faTwitter, faFacebook } from '@fortawesome/free-brands-svg-icons'
+
 import Connect from '../assets/Connect.svg'
 
 function footer () {
     return (
         <footer>
             <nav className="footerNav">
+            <Connect className="connectLogo" alt={`Connect Logo`}/>
                 <div className="socials">
-                    <Connect className="connectLogo" alt={`Connect Logo`}/>
-                    <ul>
-                        <li>
-                           <a href="https://www.meetup.com/freeCodeCamp-Nashville/" aria-label="Meetup" rel="noopener noreferrer" target="_blank">
-                               
-                           </a>
-                        </li>
-                        <li>
-                           <a href="#discord" aria-label="Discord" rel="noopener noreferrer" target="_blank">
-                               
-                           </a> 
-                        </li>
-                        <li>
-                           <a href="https://nashdev.com/" aria-label="Slack" rel="noopener noreferrer" target="_blank">
-                               
-                           </a> 
-                        </li>
-                        <li>
-                           <a href="https://github.com/nashvillefcc/" aria-label="GitHub" rel="noopener noreferrer" target="_blank">
-                               
-                           </a> 
-                        </li>
-                        <li>
-                           <a href="https://twitter.com/nashvillefcc/" aria-label="Twitter" rel="noopener noreferrer" target="_blank">
-                               
-                           </a> 
-                        </li>
-                        <li>
-                           <a href="https://www.facebook.com/groups/free.code.camp.nashville/permalink/1479740658703738/" aria-label="Facebook" rel="noopener noreferrer" target="_blank">
-                               
-                           </a> 
-                        </li>
-                    </ul>
+                    <a href="https://www.meetup.com/freeCodeCamp-Nashville/" aria-label="Meetup" rel="noopener noreferrer" target="_blank">
+                        <FontAwesomeIcon icon={faMeetup} size="1x" color="#444444"/>
+                    </a>
+                    <a href="https://discord.gg/cX9BkKrAPV" aria-label="Discord" rel="noopener noreferrer" target="_blank">
+                        <FontAwesomeIcon icon={faDiscord} size="1x" color="#444444"/> 
+                    </a> 
+                    <a href="https://nashdev.com/" aria-label="Slack" rel="noopener noreferrer" target="_blank">
+                        <FontAwesomeIcon icon={faSlack} size="1x" color="#444444"/> 
+                    </a> 
+                    <a href="https://github.com/nashvillefcc/" aria-label="GitHub" rel="noopener noreferrer" target="_blank">
+                        <FontAwesomeIcon icon={faGithub} size="1x" color="#444444"/> 
+                    </a> 
+                    <a href="https://twitter.com/nashvillefcc/" aria-label="Twitter" rel="noopener noreferrer" target="_blank">
+                        <FontAwesomeIcon icon={faTwitter} size="1x" color="#444444"/>
+                    </a> 
+                    <a href="https://www.facebook.com/groups/free.code.camp.nashville/permalink/1479740658703738/" aria-label="Facebook" rel="noopener noreferrer" target="_blank">
+                        <FontAwesomeIcon icon={faFacebook} size="1x" color="#444444"/> 
+                    </a> 
                 </div>
                 <div className="emailAndCopyright">
+                    <div>
+                        <a href="mailto:setha@hey.com" className="emailLink">
+                            <FontAwesomeIcon icon={faEnvelope} size="2x" color="white"/>
+                            <p className="emailName">setha@hey.com</p>                     {/* This will change to something else once we setup a domain */}
+                        </a>
+                        <p>© 2021 FreeCodeCamp Nashville</p>
+                    </div>
 
+                    <a href="https://www.netlify.com">
+                        <img src="https://www.netlify.com/img/global/badges/netlify-color-accent.svg" alt="Deploys by Netlify" />
+                    </a>
                 </div>
             </nav>
         </footer>
@@ -50,12 +50,3 @@ function footer () {
 }
 
 export default footer
-
-
-
-
-/*
-flex flex-col justify-between max-w-4xl h-64 p-0 mx-auto text-sm md:p-8
-flex flex-row h-1/2 w-full border-2
-flex flex-col h-1/2 border-2
-*/
