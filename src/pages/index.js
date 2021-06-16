8a37065b0ebf49679342c8807ea3cf024e4a25cd
Reid Fuhrman <reidjf77@gmail.com>
2021-06-16 16:19:29 -0400

    Initial Commit
    My goal is to copy over the work that I did on my forked repository, then link this branch to another netlify site.

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,6 +15,38 @@ function IndexPage() {
     getEvents();
   }, []);
 
+  useEffect(() => {
+    fetch(
+      // 'https://sleepy-villani-37e19c.netlify.app/.netlify/functions/pingEvents'
+      'http://localhost:8888/.netlify/functions/pingEvents'
+    )
+      .then(res => res.json())
+      .then(data => {
+        console.log(data.msg);
+
+        let date_regex = /\w+(day), \w+ \d{1,2} (at) \d{1,2}:\d{1,2} \w+/g; // assumes this is the standard date format and will not change
+
+        // need date, title, picture, and event type (Online or in-person)
+        // ask about caching (browser or through functions??)
+
+        let { items } = data.msg;
+        items.forEach(item => {
+          let date_matches = item.content.match(date_regex);
+          let ID_matches = item.content.match(/\d{9}/g); // assuming the id is always a nine digit number
+
+          let event = {
+            id: ID_matches ? ID_matches[0] : 'no id found...',
+            time: date_matches ? date_matches[0] : 'no date found...',
+            name: item.title,
+            thumbnail: item.thumbnail,
+            is_online_event: item.content.includes('online'), // for now, I don't really see a better way to obtain this info, as there isn't any attribute that specifies this
+          };
+
+          console.log(event);
+        });
+      });
+  }, []);
+
   return (
     <Layout>
       <SEO keywords={[`nashville`, `freecodecamp`]} title="Home" />
